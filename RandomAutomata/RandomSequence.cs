0a88e81bababf2bf6d18fde75491b0d805c57fe5
Izzette <aircowpeacock@gmail.com>
2015-02-15 17:29:11 -0600

    Major: Backwards incompatable changes; RandomSequence.BoolsLength and RandomSequence.BytesLength now static members.  Added to RandomSequence; specified length overloads for GetNextBools and GetNextBytes.

--- RandomAutomata/RandomSequence.cs
+++ RandomAutomata/RandomSequence.cs
@@ -1,3 +1,24 @@
+// 
+// RandomSequence.cs
+// 
+// Author:
+//     Isabell Cowan <isabellcowan@gmail.com>
+//
+// Copyright (c) 2015 Isabell Cowan
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 using System;
 using System.Threading.Tasks;
 
@@ -5,23 +26,20 @@ namespace RandomAutomata
 {
 	public class RandomSequence
 	{
+		
+		public RandomSequence ()
+		{
+			ulong seedNumber = GetRandomSeed ();
+			this.Init (seedNumber);
+		}
+
 		public RandomSequence (ulong seedNumber)
 		{
-			this.seedNumber = seedNumber;
-			byte[] seedArray = new byte[automataLength];
-			for (int i = 0; i < automataLength; i++) {
-				seedArray [i] = (byte)(seedNumber % 2);
-				seedNumber >>= 1;
-			}
-			this.automata = new Automata (seedArray);
+			this.Init (seedNumber);
 		}
 
 		public ulong SeedNumber {
 			get { return this.seedNumber; }
-		}
-
-		public int BoolsLength {
-			get { return automataLength; }
 		}
 
 		public bool[] GetNextBools ()
@@ -39,22 +57,56 @@ namespace RandomAutomata
 			return nextBools;
 		}
 
-		public int BytesLength {
-			get { return automataLength / byteLength; }
+		public bool[] GetNextBools (int length)
+		{
+			bool[] nextBools = new bool[length];
+			int quotient = length / BoolsLength;
+			if (0 != quotient) {
+				Parallel.For (0, quotient, i => {
+					this.GetNextBools ().CopyTo (nextBools, i * automataLength);
+				});
+			}
+			int remainder = length % BoolsLength;
+			if (0 != remainder) {
+				bool[] sequence = this.GetNextBools ();
+				Parallel.For (0, remainder, i => {
+					nextBools [length - i] = sequence [i];
+				});
+			}
+			return nextBools;
 		}
 
 		public byte[] GetNextBytes ()
 		{
 			this.Skip (TimeSpace);
 			byte[] states = this.automata.States;
-			byte[] nextBytes = new byte[automataLength / byteLength];
+			byte[] nextBytes = new byte[automataLength / lengthOfByte];
 			Parallel.For (0, nextBytes.Length, i => {
-				for (int ie = 0; ie < byteLength; ie++) {
+				for (int ie = 0; ie < lengthOfByte; ie++) {
 					nextBytes [i] <<= 1;
-					int statesIndex = (i * byteLength) + ie;
+					int statesIndex = (i * lengthOfByte) + ie;
 					nextBytes [i] |= states [statesIndex];
 				}
 			});
+			return nextBytes;
+		}
+
+		public byte[] GetNextBytes (int length)
+		{
+			byte[] nextBytes = new byte[length];
+			int quotient = length / BytesLength;
+			if (0 != quotient) {
+				Parallel.For (0, quotient, i => {
+					this.GetNextBytes ().CopyTo (nextBytes, i * automataLength);
+				});
+			}
+			int remainder = length % BytesLength;
+			if (0 != remainder) {
+				byte[] sequence = this.GetNextBytes ();
+				Parallel.For (0, remainder, i => {
+					nextBytes [length - i] = sequence [i];
+				});
+			}
 			return nextBytes;
 		}
 
@@ -65,7 +117,7 @@ namespace RandomAutomata
 			ulong nextULong = 0;
 			for (int i = 0; i < automataLength; i++) {
 				nextULong <<= 1;
-				int statesIndex = (i * byteLength) + i;
+				int statesIndex = (i * lengthOfByte) + i;
 				nextULong += (ulong)states [statesIndex];
 			}
 			return nextULong;
@@ -78,7 +130,7 @@ namespace RandomAutomata
 			long nextLong = 0;
 			for (int i = 0; i < automataLength; i++) {
 				nextLong <<= 1;
-				int statesIndex = (i * byteLength) + i;
+				int statesIndex = (i * lengthOfByte) + i;
 				nextLong += (long)states [statesIndex];
 			}
 			return nextLong;
@@ -91,12 +143,50 @@ namespace RandomAutomata
 			}
 		}
 
+		public static int BoolsLength {
+			get { return automataLength; }
+		}
+
+		public static int BytesLength {
+			get { return automataLength / lengthOfByte; }
+		}
+
 		private ulong seedNumber;
 		private Automata automata;
 
+		private void Init (ulong seedNumber)
+		{
+			this.seedNumber = seedNumber;
+			byte[] seedArray = GetSeedArray (seedNumber);
+			this.automata = new Automata (seedArray);
+		}
+
+		private byte[] GetSeedArray (ulong seedNumber)
+		{
+			byte[] seedArray = new byte[automataLength];
+			for (int i = 0; i < automataLength; i++) {
+				seedArray [i] = (byte)(seedNumber % 2);
+				seedNumber >>= 1;
+			}
+			return seedArray;
+		}
+
 		private const int automataLength = 64;
 		private const int TimeSpace = 4;
-		private const int byteLength = 8;
+		private const int lengthOfByte = 8;
+
+		private static ulong GetRandomSeed ()
+		{
+			Random random = new Random ();
+			ulong seed = 0;
+			for (int i = 0; i < 2; i++) {
+				seed <<= 32;
+				seed |= (ulong)(random.Next ());
+				seed <<= 1;
+				seed |= (ulong)(random.Next (0, 2));
+			}
+			return seed;
+		}
 
 	}
 }
